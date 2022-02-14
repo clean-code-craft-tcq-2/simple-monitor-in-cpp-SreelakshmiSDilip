20649461be3563501628f83accca6444c00b9232
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-02-14 23:18:12 +0530

    Update BatteryStateChecker.cpp

--- BatteryStateChecker.cpp
+++ BatteryStateChecker.cpp
@@ -7,7 +7,7 @@ char alertString[100] = "Alert: Battery Status: Out of Range: ";
 int (*fpPrint) (const char*) = &showBatteryStatus;
 
 int showBatteryStatus(const char* statement) {
-	cout<<" \n" << statement);
+	cout<<" \n" << statement;
 	return 0;
 }
 
