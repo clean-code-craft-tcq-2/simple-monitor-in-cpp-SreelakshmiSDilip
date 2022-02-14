cc132eaa246adad4341757a573e691738573f1d5
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-02-14 22:54:51 +0530

    Update BatteryStateChecker.h

--- BatteryStateChecker.h
+++ BatteryStateChecker.h
@@ -5,9 +5,9 @@
 #define MAX_THRESHOLD_CHARGE_RATE 0.8
 
 
-extern int checkBatteryTemp(float temperature);
-extern int checkBatterySoC(float SoC);
-extern int checkBatteryChargeRate(float chargeRate);
-extern int checkBatteryStatus(float temperature, float SoC, float chargeRate);
-int showBatteryStatus(const char* statement);
-extern int (*fpPrint) (const char*);
+ bool getBatteryTempStatus(float temperature);
+ bool getBatterySoCStatus(float SoC);
+ bool getBatteryChargingRateStatus(float chargeRate);
+ bool getOverallBatteryStatus(float temperature, float SoC, float chargeRate);
+ int showBatteryStatus(const char* statement);
+ int (*fpPrint) (const char*);
