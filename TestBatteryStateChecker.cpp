d0d62358f0ac48b2fbde2d7e5d4db0e4d9ed0b20
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-02-14 23:07:54 +0530

    Update TestBatteryStateChecker.cpp

--- TestBatteryStateChecker.cpp
+++ TestBatteryStateChecker.cpp
@@ -2,13 +2,6 @@
 #include <assert.h>
 #include "BatteryStateChecker.h"
 
-int printCallCount = 0;
-
-int testShowBatteryStatus(const char* statement) {
-	printf("%s \n",statement);
-	printCallCount++;
-	return 0;
-}
 
 void testGetBatteryTempStatus(float temperature, int expectedStatus) {
 	assert(getBatteryTempStatus(temperature) == expectedStatus);
@@ -27,16 +20,16 @@ void testGetOverallBatteryStatus(float temperature, float SoC, float chargeRate,
 }
 
 int main() {
-        fpPrint = &testShowBatteryStatus; //Assigning fake print function for testing
+        fpPrint = &testShowBatteryStatus; 
 	testGetBatteryTempStatus(30, 1);
 	testGetBatteryTempStatus(55, 0);
 	testGetBatterySoCStatus(50, 1);
 	testGetBatterySoCStatus(85, 0);
-	testGetBatteryChargingRateStatus(0.7, 1);
+	testGetBatteryChargingRateStatus(0.6, 1);
 	testGetBatteryChargingRateStatus(0.9, 0);
 	testGetOverallBatteryStatus(20, 45, 0.6, 1);
 	testGetOverallBatteryStatus(55, 100, 0.9, 0);
 	testGetOverallBatteryStatus(43, 90, 0.6, 0);
 	testGetOverallBatteryStatus(43, 70, 0.9, 0);
-	assert(printCallCount == 6);
+
 }
