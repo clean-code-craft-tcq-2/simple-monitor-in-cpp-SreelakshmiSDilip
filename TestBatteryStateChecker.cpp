ba30bbc9431e864c5a1e9a61cbd9cafa75781653
SreelakshmiSDilip <91017915+SreelakshmiSDilip@users.noreply.github.com>
2022-02-15 14:05:57 +0530

    Update TestBatteryStateChecker.cpp

--- TestBatteryStateChecker.cpp
+++ TestBatteryStateChecker.cpp
@@ -20,15 +20,15 @@ void testGetOverallBatteryStatus(float temperature, float SoC, float chargeRate,
 }
 
 int main() {
-	testGetBatteryTempStatus(30, 1 ,'C');
-	testGetBatteryTempStatus(122, 0 ,'F' );
+	testGetBatteryTempStatus(30, (1 ,'C'));
+	testGetBatteryTempStatus(122, (0 ,'F') );
 	testGetBatterySoCStatus(24, 1);
 	testGetBatterySoCStatus(85, 0);
 	testGetBatteryChargingRateStatus(0.6, 1);
 	testGetBatteryChargingRateStatus(0.9, 0);
-	testGetOverallBatteryStatus(20, 'C',45, 0.6, 1);
-	testGetOverallBatteryStatus(122,'F', 90, 0.9, 0);
-	testGetOverallBatteryStatus(40,'C', 97, 0.6, 0);
-	testGetOverallBatteryStatus(40,'C', 77, 0.9, 0);
+	testGetOverallBatteryStatus((20, 'C'),45, 0.6, 1);
+	testGetOverallBatteryStatus((122,'F'), 90, 0.9, 0);
+	testGetOverallBatteryStatus((40,'C'), 97, 0.6, 0);
+	testGetOverallBatteryStatus((40,'C'), 77, 0.9, 0);
 
 }
